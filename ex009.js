4104d819f8bbbff1a8f9bac077a262641c05a7fc
Diego Samuel Pereira Fagundes <zdiegofagundes@gmail.com>
2024-08-06 19:08:11 -0300

    ex 009 ex011 ex012

--- ex009.js
+++ ex009.js
@@ -2,7 +2,7 @@ var pais = 'EUA'
 
 console.log(`Vivendo em ${pais}`)
 if (pais == 'Brasil') {
-    console.log(' vc e Brasileiro')
+    console.log('vc e Brasileiro')
 } else {
     console.log('vc e Estrangeiro')
 }
