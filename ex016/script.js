df929e4bd0432a73a9c3688513d7ad8e135fdc57
Diego Samuel Pereira Fagundes <zdiegofagundes@gmail.com>
2024-08-17 19:41:55 -0300

    Update script js

--- ex016/script.js
+++ ex016/script.js
@@ -7,8 +7,25 @@ function contar(){
     if (ini.value.length == 0  || fim.value.lenght == 0 || passo.value.length == 0){
         window.alert('[ERRO]Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo invalido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f){
+            //Contagem crescente
+            for(let c = i ; c <= f ; c += p ){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for(let c = i;c >= f;c -= p)
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1}`
     }
 }
 
